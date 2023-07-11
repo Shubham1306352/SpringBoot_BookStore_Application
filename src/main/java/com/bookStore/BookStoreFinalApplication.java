52a23d3e9a8b7edb16afefc9d7d69ad218ec2bcb
Shubham <shubham1306352@gmail.com>
2023-07-12 01:42:05 +0530

    first commit

---
+++ src/main/java/com/bookStore/BookStoreFinalApplication.java
@@ -0,0 +1,13 @@
+package com.bookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreFinalApplication.class, args);
+	}
+
+}
